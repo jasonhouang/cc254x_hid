80ed22e013e883f0e457a93b81561fb9782224aa
adam.huang <adam.huang@mrobot.cn>
2017-08-31 21:44:10 +0800

    first commit, install files from BLE-CC254x-1.4.2.2.exe

---
+++ Components/hal/target/CC2541ARC/stdint.h
@@ -0,0 +1,66 @@
+/******************************************************************************
+
+ @file  stdint.h
+
+ @brief 
+
+ Group: WCS, BTS
+ Target Device: CC2540, CC2541
+
+ ******************************************************************************
+ 
+ Copyright (c) 2010-2016, Texas Instruments Incorporated
+ All rights reserved.
+
+ IMPORTANT: Your use of this Software is limited to those specific rights
+ granted under the terms of a software license agreement between the user
+ who downloaded the software, his/her employer (which must be your employer)
+ and Texas Instruments Incorporated (the "License"). You may not use this
+ Software unless you agree to abide by the terms of the License. The License
+ limits your use, and you acknowledge, that the Software may not be modified,
+ copied or distributed unless embedded on a Texas Instruments microcontroller
+ or used solely and exclusively in conjunction with a Texas Instruments radio
+ frequency transceiver, which is integrated into your product. Other than for
+ the foregoing purpose, you may not use, reproduce, copy, prepare derivative
+ works of, modify, distribute, perform, display or sell this Software and/or
+ its documentation for any purpose.
+
+ YOU FURTHER ACKNOWLEDGE AND AGREE THAT THE SOFTWARE AND DOCUMENTATION ARE
+ PROVIDED “AS IS” WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY, TITLE,
+ NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL
+ TEXAS INSTRUMENTS OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER CONTRACT,
+ NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR OTHER
+ LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+ INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE
+ OR CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT
+ OF SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+ (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+
+ Should you have any questions regarding your right to use this Software,
+ contact Texas Instruments Incorporated at www.TI.com.
+
+ ******************************************************************************
+ Release Name: ble_sdk_1.4.2.2
+ Release Date: 2016-06-09 06:57:09
+ *****************************************************************************/
+
+ // If the compiler have not this file
+#ifndef __STDINT_H
+#define __STDINT_H
+
+
+// 8 bits
+typedef	signed   char     int8_t;
+typedef	unsigned char	    uint8_t;
+
+// 16bits
+typedef signed   short    int16_t;
+typedef	unsigned short    uint16_t;
+
+// 32 bits
+typedef signed   long   int32_t;
+typedef unsigned long   uint32_t;
+
+
+#endif // __STDINT_H
